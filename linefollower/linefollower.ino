de239b84d415d1d0ff27adada6db95c0bff41537
Aaron Miller <acmiller273@gmail.com>
2016-01-23 13:43:13 -0500

    Added serial logging with PID loop at constant 1kHz

--- linefollower/linefollower.ino
+++ linefollower/linefollower.ino
@@ -2,21 +2,20 @@
 
 #define SENSOR_FIRST_PIN A0
 #define NUM_SENSORS 8
-#define MOTORA_PWM_PIN 6
-#define MOTORB_PWM_PIN 5
-#define MOTORA_1_PIN 7
-#define MOTORA_2_PIN 8
-#define MOTORB_1_PIN 10
-#define MOTORB_2_PIN 9
+#define MOTORL_PWM_PIN 6
+#define MOTORR_PWM_PIN 9
+#define MOTORL_1_PIN 8
+#define MOTORL_2_PIN 10
+#define MOTORR_1_PIN 12
+#define MOTORR_2_PIN 11
 
-#define MOTOR_KP 10
+#define SERVO_PIN 5
+
+#define MOTOR_KP 2
 #define MAX_STRAIGHT_PWM 180
 #define MAX_TURN_PWM 255
 
-#define SERVO_KP 0.2
-#define SENSOR_KP 0.2
-
-#define SERVO_OFFSET 20 //Our software thinks that 0 degrees is center but this is off, this is a fudge factor
+#define SERVO_KP 0.03
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -27,147 +26,156 @@
 
 Servo servo;
 float servo_position = 0;
+float analog_cutoff = 200;
 
-float atan_lookup[17];
-
+IntervalTimer pid_timer;
 void setup()
 {
-   Serial.begin(9600);
+  Serial1.begin(9600);
 
-  servo.attach(12);
+  servo.attach(SERVO_PIN);
   servo.write(90);
 
   for (int i = 0; i < NUM_SENSORS; i++) {
     pinMode(SENSOR_FIRST_PIN + i, INPUT);
   }
 
-  //Setup the arc tangent lookup table, calculating arctangent on the fly would be very slow.
-  //This makes the worse case linear since we only need certain values anyway
-  for (int i = 0; i < NUM_SENSORS*2 + 1; i++){
-    //The sensors are 5.625mm apart and 50mm from the rotation point, convert to degrees since that is what we use for the rest
-    float pos = (i-NUM_SENSORS)/2.0;
-    atan_lookup[i] = atan(pos*5.625/50) * 180.0 / PI;
-    Serial.println(atan_lookup[i]);
-  }
-
   // change ADC prescaler
-  sbi(ADCSRA,ADPS2);
-  cbi(ADCSRA,ADPS1);
-  cbi(ADCSRA,ADPS0);
+  // sbi(ADCSRA, ADPS2);
+  // cbi(ADCSRA, ADPS1);
+  // cbi(ADCSRA, ADPS0);
 
-  pinMode(MOTORA_1_PIN, OUTPUT);
-  pinMode(MOTORA_2_PIN, OUTPUT);
-  pinMode(MOTORA_PWM_PIN, OUTPUT);
-  pinMode(MOTORB_1_PIN, OUTPUT);
-  pinMode(MOTORB_2_PIN, OUTPUT);
-  pinMode(MOTORB_PWM_PIN, OUTPUT);
+  pinMode(MOTORL_1_PIN, OUTPUT);
+  pinMode(MOTORL_2_PIN, OUTPUT);
+  pinMode(MOTORL_PWM_PIN, OUTPUT);
+  pinMode(MOTORR_1_PIN, OUTPUT);
+  pinMode(MOTORR_2_PIN, OUTPUT);
+  pinMode(MOTORR_PWM_PIN, OUTPUT);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, 0);
+
+  pid_timer.begin(pid_interrupt, 1000);
 }
 
-uint8_t sensor_values = 0;
+uint16_t sensor_values = 0;
 
 // 'l' for left, 'r' for right, 's' for straight
 char servo_turn_direction = 's';
 
-void loop()
+char serial_buf[600];
+volatile bool serial_buf_ready = false;
+int serial_chars = 0;
+
+void loop() {
+  while (!serial_buf_ready) {}
+
+  int serial_chars_printed = 0;
+
+  while (serial_chars_printed < serial_chars) {
+    for (int i = 0; serial_chars_printed < serial_chars && i < 300; i++) {
+      Serial1.write(serial_buf[serial_chars_printed]);
+      serial_chars_printed++;
+    }
+    delay(30);
+  }
+
+  Serial1.write('\r');
+  Serial1.write('\n');
+  serial_chars = 0;
+  serial_buf_ready = false;
+}
+
+uint32_t t = 0;
+void pid_interrupt()
 {
-  sensor_values = PINC;
-  sensor_values |= (analogRead(A6) > 1) << 6;
-  // delay(10);
-  sensor_values |= (analogRead(A7) > 1) << 7;
+  bool print_locked = !serial_buf_ready;
+
+  if (micros() - t > 1000 && t != 0) digitalWrite(13, 1);
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%lu", micros() - t);
+  t = micros();
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "\t");
+
+  sensor_values = 0;
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    sensor_values |= (analogRead(SENSOR_FIRST_PIN + i) > analog_cutoff) << i;
+  }
 
   for (int i = 0; i < NUM_SENSORS; i++) {
-    Serial.print((sensor_values >> i) & 1);
+    if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%i", (sensor_values >> i) & 1);
   }
-  Serial.print("\t");
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "\t");
 
-  //Start from leftmost side, search till we find the point before the line starts (looking for zeros)
-  //If the line is right to the edge the value returned will be -1, if the line is not found it will be 7
   int left_side_of_line = 0;
   while (sensor_values & _BV(7 - left_side_of_line) && left_side_of_line < NUM_SENSORS) {
     left_side_of_line++;
   }
-  left_side_of_line--; //Decrement by one so 0 = leftmost
+  left_side_of_line--;
 
-  //Start from rightmost side, search till we find the point before the line starts (looking for zeros)
-  //If the line is right to the edge the value returned will be 8, if the line is not found it will be 0
-  int right_side_of_line = NUM_SENSORS-1;
+  int right_side_of_line = NUM_SENSORS - 1;
   while (sensor_values & _BV(7 - right_side_of_line) && right_side_of_line >= 0) {
     right_side_of_line--;
   }
-  right_side_of_line++;
 
-  int line_width = right_side_of_line - left_side_of_line; //Not used yet
-  float center = ((right_side_of_line + left_side_of_line) / 2.0) - 3.5;   //-3.5 = NUM_SENSORS-1/2
+  float center = (right_side_of_line + left_side_of_line - NUM_SENSORS + 1) / 2.0;
 
-  //Center will only be in 0.5 increments because of the resolution of our sensors
-  float angle_sensors = atan_lookup[(int)(center*2) + NUM_SENSORS];
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%i\t", left_side_of_line);
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%i\t", right_side_of_line);
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%f\t", center);
 
-  Serial.print(left_side_of_line);
-  Serial.print("\t");
-  Serial.print(right_side_of_line);
-  Serial.print("\t");
-  Serial.print(center);
-  Serial.print("\t");
-  Serial.print(angle_sensors);
-  Serial.print("\t");
+  if (left_side_of_line > right_side_of_line) {
+    // entire view is white
+    // turn in the same direction we were turning before
+    switch (servo_turn_direction) {
+      case 'l':
+        servo_position = constrain(servo_position - SERVO_KP*NUM_SENSORS, -90, 90);
+        break;
+      case 'r':
+      default:
+        servo_position = constrain(servo_position + SERVO_KP*NUM_SENSORS, -90, 90);
+        break;
+    }
+  } else if (left_side_of_line == -1 && right_side_of_line == NUM_SENSORS - 1) {
+    // entire view is black
+    if (servo_position < 0) {
+      servo_position += SERVO_KP*NUM_SENSORS;
+      servo_turn_direction = 'r';
+    } else {
+      servo_position -= SERVO_KP*NUM_SENSORS;
+      servo_turn_direction = 'l';
+    }
+  } else {
+    servo_position = constrain(servo_position + SERVO_KP*center, -90, 90);
+    if (center > 0) {
+      servo_turn_direction = 'r';
+    } else if (center == 0) {
+      servo_turn_direction = 's';
+    } else {
+      servo_turn_direction = 'l';
+    }
+  }
 
-//  if (left_side_of_line > right_side_of_line) {
-//    // entire view is white
-//    // turn in the same direction we were turning before
-//    switch (servo_turn_direction) {
-//      case 'l':
-//        servo_position = constrain(servo_position - SERVO_KP*NUM_SENSORS, -90, 90);
-//        break;
-//      case 'r':
-//      default:
-//        servo_position = constrain(servo_position + SERVO_KP*NUM_SENSORS, -90, 90);
-//        break;
-//    }
-//  } else if (left_side_of_line == -1 && right_side_of_line == NUM_SENSORS) {
-//    // entire view is black
-//    if (servo_position < 0) {
-//      servo_position += SERVO_KP*NUM_SENSORS;
-//      servo_turn_direction = 'r';
-//    } else {
-//      servo_position -= SERVO_KP*NUM_SENSORS;
-//      servo_turn_direction = 'l';
-//    }
-//  } else {
-//    servo_position = constrain(servo_position + SERVO_KP*center, -90, 90);
-//    if (center > 0) {
-//      servo_turn_direction = 'r';
-//    } else if (center == 0) {
-//      servo_turn_direction = 's';
-//    } else {
-//      servo_turn_direction = 'l';
-//    }
-//  }
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%f\t", servo_position);
 
-  Serial.print(servo_position);
-  Serial.print("\t");
+  //servo.write(servo_position + 90);
 
-  servo.write(servo_position + 90 + SERVO_OFFSET);
+  digitalWrite(MOTORL_1_PIN, 0);
+  digitalWrite(MOTORL_2_PIN, 1);
+  digitalWrite(MOTORR_1_PIN, 0);
+  digitalWrite(MOTORR_2_PIN, 1);
 
-  digitalWrite(MOTORA_1_PIN, 0);
-  digitalWrite(MOTORA_2_PIN, 1);
-  digitalWrite(MOTORB_1_PIN, 0);
-  digitalWrite(MOTORB_2_PIN, 1);
+  analogWrite(MOTORL_PWM_PIN, constrain(MAX_STRAIGHT_PWM + MOTOR_KP*servo_position, 0, MAX_TURN_PWM));
+  analogWrite(MOTORR_PWM_PIN, constrain(MAX_STRAIGHT_PWM - MOTOR_KP*servo_position, 0, MAX_TURN_PWM));
 
-  //analogWrite(MOTORA_PWM_PIN, constrain(MAX_STRAIGHT_PWM + MOTOR_KP*servo_position + SENSOR_KP*angle_sensors, 0, MAX_TURN_PWM));
-  //analogWrite(MOTORB_PWM_PIN, constrain(MAX_STRAIGHT_PWM - MOTOR_KP*servo_position + SENSOR_KP*angle_sensors, 0, MAX_TURN_PWM));
-
-  //Sum the servo position and the sensor position by seperate Kp's
-  Serial.print(constrain(MAX_STRAIGHT_PWM + MOTOR_KP*servo_position + SENSOR_KP*angle_sensors, 0, MAX_TURN_PWM));
-  Serial.print("\t");
-  Serial.print(constrain(MAX_STRAIGHT_PWM - MOTOR_KP*servo_position + SENSOR_KP*angle_sensors, 0, MAX_TURN_PWM));
-  Serial.print("\t");
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%f\t", constrain(MAX_STRAIGHT_PWM + MOTOR_KP*servo_position, 0, MAX_TURN_PWM));
+  if (print_locked) serial_chars += sprintf(serial_buf + serial_chars, "%f\t", constrain(MAX_STRAIGHT_PWM - MOTOR_KP*servo_position, 0, MAX_TURN_PWM));
 
   if (MAX_STRAIGHT_PWM + MOTOR_KP*servo_position <= 0) {
-    digitalWrite(MOTORA_2_PIN, 0);
+    digitalWrite(MOTORL_2_PIN, 0);
   }
   if (MAX_STRAIGHT_PWM - MOTOR_KP*servo_position <= 0) {
-    digitalWrite(MOTORB_2_PIN, 0);
+    digitalWrite(MOTORR_2_PIN, 0);
   }
 
-  Serial.println();
+  if (print_locked) serial_buf_ready = true;
 }
